f04a8cea471774af0f8dca2e9a805051e745f9c1
kwabla-1 <martinkwablaahedor@gmail.com>
2022-08-25 12:48:09 -1200

    worked on making the navbar responsive, included alplinejs to handle the appearing and disappearing of the dropdown nav

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+
+
+// window.onclick = function(event) {
+//      if (!triggerElement) {
+//        const dropdowns = document.getElementsByClassName("dropdown-content");
+//        var i;
+//        for (i = 0; i < dropdowns.length; i++) {
+//          var openDropdown = dropdowns[i];
+//          if (openDropdown.classList.contains('show')) {
+//            openDropdown.classList.remove('show');
+//          }
+//        }
+//      }
+// }
